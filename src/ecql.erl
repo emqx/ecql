eeee26920ce068a7b3a588d5c3fcf71d5eaca0e4
Feng <ery.lee@gmail.com>
2015-12-21 10:52:46 +0800

    connect

--- src/ecql.erl
+++ src/ecql.erl
@@ -19,24 +19,99 @@
 %%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 %%% SOFTWARE.
 %%%-----------------------------------------------------------------------------
-%%% @doc CQL Client.
+%%% @doc CQL Driver.
 %%%
 %%% @author Feng Lee <feng@emqtt.io>
 %%%-----------------------------------------------------------------------------
 -module(ecql).
 
-%% ecql: ecql library's entry point.
+-behaviour(gen_fsm).
 
--export([start_link/0]).
+-include("ecql.hrl").
 
-%% API
+-include("ecql_frame.hrl").
 
-start_link() ->
-    ok().
+-import(proplists, [get_value/2, get_value/3]).
 
-%% Internals
+%% API Function Exports
+-export([start_link/1, start_link/2]).
 
-ok() ->
+%% gen_fsm Function Exports
+-export([connected/2, connected/3, disconnected/2, disconnected/3]).
+
+-export([init/1, handle_event/3, handle_sync_event/4, handle_info/3,
+         terminate/3, code_change/4]).
+
+-type host() :: inet:ip_address() | inet:hostname().
+
+-type option() :: {host, host()}
+                | {port, inet:port_number()}
+                | {ssl,  boolean()}
+                | {ssl_opts, [ssl:ssl_option()]}
+                | {timeout,  timeout()}
+                | {logger, atom() | {atom(), atom()}}.
+
+-record(state, {nodes     :: [{host(), inet:port_number()}],
+                transport :: tcp | ssl,
+                socket    :: inet:socket(),
+                receiver  :: pid(),
+                requests  :: dict:dict(),
+                logger    :: gen_logger:logmod(),
+                tcp_opts  :: [gen_tcp:connect_option()]}).
+
+%% API Function Definitions
+
+-spec start_link([option()]) -> {ok, pid()} | {error, any()}.
+start_link(Opts) ->
+    start_link(Opts, []).
+
+-spec start_link([option()], [gen_tcp:connect_option()])
+        -> {ok, pid()} | {error, any()}.
+start_link(Opts, TcpOpts) ->
+    case gen_fsm:start_link(?MODULE, [Opts], []) of
+        {ok, Pid} ->
+            connect(Pid, TcpOpts);
+        {error, Error} ->
+            {error, Error}
+    end.
+
+connect(Pid, TcpOpts) ->
+    case gen_fsm:sync_send_event(Pid, {connect, TcpOpts}) of
+        ok             -> {ok, Pid};
+        {error, Error} -> {error, Error}
+    end.
+
+%% gen_fsm Function Definitions
+
+init([Opts, TcpOpts]) ->
+    {ok, connected, #state{}}.
+
+connected(_Event, State) ->
+    {next_state, state_name, State}.
+
+connected(_Event, _From, State) ->
+    {reply, ok, state_name, State}.
+
+disconnected(_Event, State) ->
+    {next_state, state_name, State}.
+
+disconnected(_Event, _From, State) ->
+    {reply, ok, state_name, State}.
+
+handle_event(_Event, StateName, State) ->
+    {next_state, StateName, State}.
+
+handle_sync_event(_Event, _From, StateName, State) ->
+    {reply, ok, StateName, State}.
+
+handle_info(_Info, StateName, State) ->
+    {next_state, StateName, State}.
+
+terminate(_Reason, _StateName, _State) ->
     ok.
 
-%% End of Module.
+code_change(_OldVsn, StateName, State, _Extra) ->
+    {ok, StateName, State}.
+
+%% Internal Function Definitions
+
