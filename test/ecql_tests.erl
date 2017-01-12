3dbaf8844e1fa6ef952b9113d1b1c7c1e3baae6c
huangdan <wang_yi20081014@163.com>
2017-01-12 09:25:17 +0800

    Copyright (c) 2015-2017

--- test/ecql_tests.erl
+++ test/ecql_tests.erl
@@ -1,4 +1,4 @@
-%%% Copyright (c) 2016 eMQTT.IO, All Rights Reserved.
+%%% Copyright (c) 2016-2017 eMQTT.IO, All Rights Reserved.
 %%%
 %%% Permission is hereby granted, free of charge, to any person obtaining a copy
 %%% of this software and associated documentation files (the "Software"), to deal
